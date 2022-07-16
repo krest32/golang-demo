5c268c810157a103cf489162c2d0bc17e26d9835
krest2020 <18635572970@163.com>
2022-07-16 13:31:58 +0800

    add git igonre

--- mock/saic-cap/main/SaicBoot.go
+++ mock/saic-cap/main/SaicBoot.go
@@ -17,7 +17,7 @@ func main() {
 		// 等待30秒
 		//time.Sleep(10 * time.Second)
 		// 直接返回string信息
-		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\demo-basic\\mock\\saic-cap\\rescourses\\demoCompStr1.json"))
+		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\golang-demo\\mock\\saic-cap\\rescourses\\demoCompStr1.json"))
 	})
 
 	r.POST("/EE/saicIndi", func(c *gin.Context) {
@@ -27,14 +27,14 @@ func main() {
 		//time.Sleep(10 * time.Second)
 
 		// 直接返回string信息
-		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\demo-basic\\mock\\saic-cap\\rescourses\\demoIndiStr1.json"))
+		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\golang-demo\\mock\\saic-cap\\rescourses\\demoIndiStr1.json"))
 	})
 
 	r.POST("/EE/CAP/Token", func(c *gin.Context) {
 		// 获取请求头内容
 		util.DealContext(c)
 		// 直接返回string信息
-		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\demo-basic\\mock\\saic-cap\\rescourses\\CapToken.json"))
+		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\golang-demo\\mock\\saic-cap\\rescourses\\CapToken.json"))
 	})
 
 	r.POST("/EE/CAP/Detail", func(c *gin.Context) {
@@ -43,7 +43,7 @@ func main() {
 		// 等待30秒
 		//time.Sleep(5*time.Second)
 		// 直接返回string信息
-		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\demo-basic\\mock\\saic-cap\\rescourses\\CapDetail.json"))
+		c.String(http.StatusOK, util.GetFileString("F:\\project\\go\\GoPath\\src\\golang-demo\\mock\\saic-cap\\rescourses\\CapDetail.json"))
 	})
 
 	r.Run(":8001")
